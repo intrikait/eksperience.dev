7b6ec4fcaf57fbff6ad3cae666a219d008a19506
Kait <kaitrynshayne@gmail.com>
2016-05-05 21:05:51 -0700

    uploaded some images

--- wp-content/themes/hitchcock/content.php
+++ wp-content/themes/hitchcock/content.php
@@ -1,6 +1,6 @@
-<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'post-thumb' ); ?>
+<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($adventure->ID), 'post-thumb' ); ?>
 
-<a href="<?php the_permalink(); ?>" id="post-<?php the_ID(); ?>" <?php post_class('post'); ?> style="background-image: url(<?php echo $thumb['0']; ?>);">
+<a href="<?php the_permalink(); ?>" id="adventure-<?php the_ID(); ?>" <?php post_class('adventure'); ?> style="background-image: url(<?php echo $thumb['0']; ?>);">
 
 	<div class="post-overlay">
 		
